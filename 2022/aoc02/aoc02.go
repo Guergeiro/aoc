85aba1f2835cc9f6a2d71b3eefadff332645a501
Breno Salles <git@brenosalles.com>
2022-12-02 16:27:50 +0000

    feat(2022): Day 2

---
+++ 2022/aoc02/aoc02.go
@@ -0,0 +1,79 @@
+package aoc02
+
+import (
+	"fmt"
+	array "github.com/guergeiro/aoc/2022/_array"
+	reader "github.com/guergeiro/aoc/2022/_reader"
+)
+
+func Part1() {
+	arrExample := reader.ReadFileStrings("aoc02/input_example.txt")
+	arrQuestion := reader.ReadFileStrings("aoc02/input_question.txt")
+
+	fmt.Println(getSum(convertToCleanGames(arrExample)))
+	fmt.Println(getSum(convertToCleanGames(arrQuestion)))
+}
+
+func Part2() {
+	arrExample := reader.ReadFileStrings("aoc02/input_example.txt")
+	arrQuestion := reader.ReadFileStrings("aoc02/input_question.txt")
+
+	fmt.Println(getSum(extractGames(arrExample)))
+	fmt.Println(getSum(extractGames(arrQuestion)))
+}
+
+func getSum(games []string) int {
+	possiblePlays := getPossiblePlays()
+	sum := array.Reduce(games, func(acc int, cur string) int {
+		value := possiblePlays[cur]
+		return acc + value
+	}, 0)
+	return sum
+}
+
+func convertToCleanGames(input []string) []string {
+	games := []string{}
+	for _, value := range input {
+		games = append(games, value)
+	}
+  return games
+}
+
+func extractGames(input []string) []string {
+	games := []string{}
+	possibleMatchingPlays := getMatchingPlays()
+	for _, value := range input {
+    games = append(games, possibleMatchingPlays[value])
+	}
+  return games
+}
+
+func getPossiblePlays() map[string]int {
+	possiblePlays := map[string]int{
+		"A X": 4,
+		"A Y": 8,
+		"A Z": 3,
+		"B X": 1,
+		"B Y": 5,
+		"B Z": 9,
+		"C X": 7,
+		"C Y": 2,
+		"C Z": 6,
+	}
+	return possiblePlays
+}
+
+func getMatchingPlays() map[string]string {
+  matchingPlays := map[string]string {
+		"A X": "A Z",
+		"A Y": "A X",
+		"A Z": "A Y",
+		"B X": "B X",
+		"B Y": "B Y",
+		"B Z": "B Z",
+		"C X": "C Y",
+		"C Y": "C Z",
+		"C Z": "C X",
+  }
+  return matchingPlays
+}
--- 2022/main.go
+++ 2022/main.go
@@ -1,10 +1,19 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/guergeiro/aoc/2022/aoc01"
+	"github.com/guergeiro/aoc/2022/aoc02"
 )
 
 func main() {
+	fmt.Println("1")
 	aoc01.Part1()
 	aoc01.Part2()
+	fmt.Println("")
+
+	fmt.Println("2")
+	aoc02.Part1()
+	aoc02.Part2()
 }
